68570bec200c45445b3182a563a7b770923bc0f8
Cesar Saavedra <Andrs917@gmail.com>
2017-11-09 17:48:46 -0500

    Punto 1
    Codigo Bien

--- Laboratorio 3 Metodos/Script-Laboratorio 3.R
+++ Laboratorio 3 Metodos/Script-Laboratorio 3.R
@@ -7,56 +7,63 @@
 
 #--------------- Punto 1 ---------------#
 
-#----------Para n=10----------#
-e <- 5000
-n <- 10
+#---Para n=10---#
+
+B <- 5000 ## número de experimentos
+n <- 10 ## tamaño de muestra
 mu <- 5
 sd <- 1
 alpha <- 0.05
 cuantil <- qnorm(1-alpha/2)
-muestras <- replicate(e,rnorm(n,mu,sd))
 
+muestras <- replicate(B, rnorm(n, mu, sd))
 
-intervalo <- function(columna) {
-  m <- muestras[ , columna]
+calcula.el.intervalo <- function(columna) {
+  
+  m <- muestras[, columna]
+  
   sem <- sd(m)/sqrt(length(m))
+  
   lim.inf <- mean(m) - cuantil * sqrt(sd)/sqrt(n)
   lim.sup <- mean(m) + cuantil * sqrt(sd)/sqrt(n)
   
-  c(lim.inf,lim.sup)
-  
+  c(lim.inf, lim.sup)
 }
 
-mis.intervalos <- matrix(rep(1:5000),nrow=2)
+#---Cálculo de los nuevos intervalos---#
+
+mis.intervalos <- matrix(rep(0, 10000), nrow = 2) ## matriz de ceros
 
 for(i in 1:5000) {
-  mis.intervalos[,i] <- intervalo(i)
+  mis.intervalos[,i] <- calcula.el.intervalo(i)
 }
 
-mis.intervalos[ ,1:30]
+#---Gráfico---#
 
-plot(1:10, type = "n", main="Intervalo de confianza para una poblacion Normal(5,1) con n=10",
+plot(1:5000, type = "n",
      xlim = range(mis.intervalos),
      ylab = "Muestreos")
 
-abline(v = mu, lty = 2) 
+abline(v = mu, lty = 2, col=("red"))
+
 for(i in 1:5000) {
-  segments(mis.intervalos[1,i], i, mis.intervalos[2,i], i)
+  segments(mis.intervalos[1,i], i, mis.intervalos[2,i], i,
+           col = ifelse(mis.intervalos[1, i] < mu & mis.intervalos[2, i] > mu,
+                        "gray", "blue"))
 }
 
-#----------Para n=30----------#
+#---Para n=30---#
 
-#----------Para n=50----------#
+#---Para n=50---#
 
-#----------Para n=100----------#
-
+#---Para n=100---#
 
 #--------------- Punto 4 ---------------#
   
 
 
 #--------------- Punto 7 ---------------#
-x11()
+
 #---------------------------------------#
 
 
