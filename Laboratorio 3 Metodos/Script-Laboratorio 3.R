8a01d4df3888aaf626b15dd30e9b3207a8afdb19
Cesar Saavedra <Andrs917@gmail.com>
2017-11-09 14:28:35 -0500

    Punto 1

--- Laboratorio 3 Metodos/Script-Laboratorio 3.R
+++ Laboratorio 3 Metodos/Script-Laboratorio 3.R
@@ -6,27 +6,52 @@
 # Cesar A. Saavedra Vanegas
 
 #--------------- Punto 1 ---------------#
-n = 10
-media <- 5
-varianza <- 1
+e <- 5000
+n <- 10
+mu <- 5
+sd <- 1
 alpha <- 0.05
 cuantil <- qnorm(1-alpha/2)
-x <- rnorm(n,media,varianza)
+muestras <- replicate(e,rnorm(n,mu,sd))
 
-lim_inf <- media - cuantil * sqrt(varianza)/sqrt(n)
-lim_sup <- media + cuantil * sqrt(varianza)/sqrt(n)
 
-r=5000
-muchasnormales=replicate(r,x)
+intervalo <- function(columna) {
+m <- muestras[ , columna]
+sem <- sd(m)/sqrt(length(m))
+lim.inf <- mean(m) - cuantil * sqrt(sd)/sqrt(n)
+lim.sup <- mean(m) + cuantil * sqrt(sd)/sqrt(n)
 
-para30 <- muchasnormales[1:30]
-para50 <- muchasnormales[1:50]
-para100 <- muchasnormales[1:100]
+c(lim.inf,lim.sup)
 
-hist(muchasnormales, xlim = c(lim_inf,lim_sup))
-hist(para30, xlim = c(lim_inf,lim_sup))
-hist(para50, xlim = c(lim_inf,lim_sup))
-hist(para100, xlim = c(lim_inf,lim_sup))
+}
+
+mis.intervalos <- matrix(rep(0:5001),nrow=2)
+
+for(i in 1:5000) {
+  mis.intervalos[,i] <- intervalo(i)
+}
+
+mis.intervalos[ ,1:30]
+
+plot(1:10, type = "n",
+     xlim = range(mis.intervalos),
+     ylab = "Muestreos")
+
+abline(v = mu, lty = 2) 
+for(i in 1:5000) {
+  segments(mis.intervalos[1,i], i, mis.intervalos[2,i], i)
+}
+
+#mu1 <- muestras[ ,1:30]
+#mu2 <- muestras[ ,1:50]
+#mu3 <- muestras[ , 1:100]
+
+#lim_inf <- media - cuantil * sqrt(varianza)/sqrt(n)
+#lim_sup <- media + cuantil * sqrt(varianza)/sqrt(n)
+
+#para30 <- muchasnormales[1:30]
+#para50 <- muchasnormales[1:50]
+#para100 <- muchasnormales[1:100]
 
 
 #--------------- Punto 4 ---------------#
@@ -34,3 +59,8 @@ hist(para100, xlim = c(lim_inf,lim_sup))
 
 
 #--------------- Punto 7 ---------------#
+
+#---------------------------------------#
+
+
+
