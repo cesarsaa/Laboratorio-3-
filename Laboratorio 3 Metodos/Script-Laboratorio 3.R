dcb09fcfb4b9208f8a20e4d8ab78e646244b0423
Cesar Saavedra <Andrs917@gmail.com>
2017-11-09 10:24:57 -0500

    Script

---
+++ Laboratorio 3 Metodos/Script-Laboratorio 3.R
@@ -0,0 +1,15 @@
+
+# Laboratorio 3 Metodos Estadisticos 
+
+# Diana C. Arias Sinisterra
+# Kevin S. Garcia Chica
+# Cesar A. Saavedra Vanegas
+
+
+#--------------- Punto 1 ---------------#
+
+
+#--------------- Punto 4 ---------------#
+
+
+#--------------- Punto 7 ---------------#
